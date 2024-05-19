6a6138b52c1ba2c09ca974f76553ffae97c911fd
Hafsa <ali.hafsa.ahmed@gmail.com>
2024-05-19 18:32:16 -0500

    Removed checkToken

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const usersCtrl = require('../../controllers/api/users');
 const ensureLoggedIn = require('../../config/ensureLoggedIn');
 
-router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken);
 
 // POST /api/users
 router.post('/', usersCtrl.create);
--- src/pages/GroceryListPage/GroceryListPage.jsx
+++ src/pages/GroceryListPage/GroceryListPage.jsx
@@ -1,17 +1,11 @@
-import { checkToken } from '../../utilities/users-service';
+
 
 export default function GroceryListPage() {
-    async function handleCheckToken(evt) {
-        const expDate = await checkToken();
-        console.log(expDate);
-    }
+    
 
     return (
         <>
             <h1>GroceryListPage</h1>
-            <button
-                onClick={handleCheckToken}
-            >Check When My Login Expires</button>
         </>
     );
 }
